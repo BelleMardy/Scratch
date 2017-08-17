b3d272ae7cbe3cb3b02855f6d85952093b9fe819
Belle <belle.belle@my.jcu.edu.au>
2017-08-17 16:07:26 +1000

    get number and provide the same number back after a calculation

--- Scratch_1.py
+++ Scratch_1.py
@@ -1,10 +1,44 @@
-def main():
-    print("Hello Mardy")
+# def main():
+#     print("Hello Mardy")
+#
+# main()
+#
+# def main():
+#     pet_name = input("What is your pets name: ")
+#     print("Your pet's name is {:>10s}.".format(pet_name))
+#
+# main()
 
-main()
 
 def main():
-    pet_name = input("What is your pets name: ")
-    print("Your pet's name is {:>10s}.".format(pet_name))
+    number = float(input("Input a number: "))
+    number = first_addition(number)
+    print(float(number))
+    number = second_multiplication(number)
+    print(float(number))
+    number = third_subtraction(number)
+    print(float(number))
+    number = forth_subtraction(number)
+    print("This has now reverted to the number you started with, {:.2f}".format(float(number)))
+
+
+def first_addition(number):
+    number += 2
+    return number
+
+
+def second_multiplication(number):
+    number *= 3
+    return number
+
+
+def third_subtraction(number):
+    number -= 6
+    return number
+
+
+def forth_subtraction(number):
+    number /= 3
+    return number
 
 main()
